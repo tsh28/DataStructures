20f018e06e281f3a2dbd6825c900a798c9aa7a8a
ThienSon <s.hoang1103@gmail.com>
2025-01-23 00:25:45 -0500

    add some imps for SLL

---
+++ SinglyLinkedList/SinglyLinkedList.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std; 
+
+class Node { 
+public: 
+    int val; 
+    Node* next ; 
+    Node(): val(0), next(nullptr){}; 
+    Node(int val): val(val), next(nullptr) {}; 
+}; 
+
+class SinglyLinkedList{
+public: 
+    SinglyLinkedList(): head(NULL){}; 
+
+    bool isEmpty(){
+        return head == nullptr; 
+    }; 
+
+    void insertFront(int val){
+        Node* newNode = new Node(val); 
+        newNode->next = head; 
+        head = newNode; 
+        return; 
+    }; 
+
+    void insertBack(int val){
+        Node* newNode = new Node(val); 
+        Node* tmp = head; 
+        if (isEmpty()){
+            head = tmp; 
+            return; 
+        }
+        while (tmp->next){
+            tmp = tmp->next; 
+        }
+        tmp->next = newNode; 
+        return;       
+        
+    }; 
+
+    void insertAt(int val, int index){
+
+    }; 
+
+    void removeFront(){
+
+    }; 
+
+    void removeBack(){
+
+    }; 
+
+    void removeAt(int index){
+
+    }; 
+    void show(){
+        if (isEmpty()){
+            cout << "SLL is empty" << "\n"; 
+        }
+        Node* tmp_head = head; 
+        while (tmp_head){
+            cout << tmp_head->val << "->"; 
+            tmp_head = tmp_head->next; 
+        }
+        cout << "null \n"; 
+    }; 
+
+private: 
+    Node* head; 
+
+}; 
+
+int main() {
+    SinglyLinkedList SLL; 
+    SLL.insertFront(1);
+    SLL.insertBack(3);  
+    // SLL.insertBack(2); 
+    // SLL.insertAt(5,1); 
+    SLL.show(); 
+    return 0; 
+}
